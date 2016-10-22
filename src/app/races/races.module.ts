a3a5db24af7ea7bf61170d9a134b643371ef09ed
Kjetil Kyrkjerud Hårtveit <kjetil_runescape@hotmail.com>
2016-10-22 11:16:44 +0200

    Removed CoreModule import

--- src/app/races/races.module.ts
+++ src/app/races/races.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CoreModule } from '../core/core.module';
 
 import { RaceToIconPathPipe }	from './race-to-icon-path.pipe';
 import { RacesService }	from './races.service';
 
 @NgModule({
   imports: [
-    CommonModule,
-		CoreModule
+    CommonModule
   ],
   declarations: [
 		RaceToIconPathPipe
