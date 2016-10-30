facc3b835124cbd3a0fae8aea714b7ce6a68c65b
Kjetil Kyrkjerud Hårtveit <kjetil_runescape@hotmail.com>
2016-10-30 18:59:12 +0100

    Removed selectedItem from EditEntityTemplateService

--- src/app/shared/edit-entity-template/edit-entity-template.service.ts
+++ src/app/shared/edit-entity-template/edit-entity-template.service.ts
@@ -1,26 +1,9 @@
 import { Injectable, OnInit, OnDestroy } from '@angular/core';
-import { Subscription, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
-export class EditEntityTemplateService<T> implements OnInit, OnDestroy {
-	selectedItem: T;
+export class EditEntityTemplateService<T> {
 	selectItem = new BehaviorSubject<T>(null);
 
-	subs: Subscription[] = [];
-
   constructor() { }
-	
-	ngOnInit() {
-		this.subs.push(
-			this.selectItem.subscribe((item) => {
-				this.selectedItem = item;
-			})
-		);
-	}
-
-	ngOnDestroy() {
-		this.subs.forEach((sub) => {
-			sub.unsubscribe();
-		})
-	}
 }
